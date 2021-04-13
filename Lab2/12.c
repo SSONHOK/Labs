f65efe059ff8717eb723a74bfc9cce3b30895dd5
PeTNiK313 <nekit31333@yandex.ru>
2021-04-13 14:07:19 +0500

    2 Лаба добавлена

---
+++ Lab2/12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x;
+    printf (" Enter x {3.15 < x} -> ");
+    scanf ("%lf", &x);
+    if (x>3.15)
+    {
+        double y = - log(x + 2)-log(x - 2);
+        double z = sqrt(3*y - y*y*y);
+        printf("y(x)=%lf\nz(y) = %lf\n", y, z);
+    }
+    else
+        printf("x value is incorrect!\n");
+    return 0;
+}
